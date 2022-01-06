cf5c4d86244deba52cb0c9a41ad4d3d7e1224ec7
hal7bail <hal7bail@gmail.com>
2022-01-06 20:37:17 +0300

    done

--- index.js
+++ index.js
@@ -8,4 +8,46 @@ function calculateBMI() {
   console.table({ age, weight, height, male, female });
 
   // You are going to write the code here before the curly braces 👇🏻
+
+  
+let BMI = ( weight / (height/100)^2);
+alert(BMI);
+
+  if ( BMI < 18.5){
+    alert("underweight")
+  } else if ( BMI > 18.15 && BMI < 24.9) {
+    alert("healthy weight")
+  } else if ( BMI > 24.9 && BMI <= 29.9) {
+    alert ("overweight")
+  } else if ( BMI >= 30.0) {alert ("obesity")}
+
+// const bmi = weight / (height/100 **2;)
+// let bmistatus;
+
+// if (bmi < 20) {
+//   bmistatus = "thin";
+// }
+// else if (bmi < 25) {
+//   bmistatus = "normal";
+// }
+// else { bmistatus = "fat";}
+// }
+
+// let gender;
+// if (male) { gender = "male";}
+// else {gender = "female";}
+// - or
+// const or let gender = male ? "male" : "female";
+
+
+// alert(
+//   `weight: ${weight}
+//   Height:${height}
+//   age:${age}
+//   male:${male}
+//   female:${female}
+//   ---
+//   BMI is: ${bmi}
+//   your body status  is ${bmistatus}
+//   `);
 }
